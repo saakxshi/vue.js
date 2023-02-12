d67c0f641e2798e66139da013dc6fba0898bc6dc
saakxshi <saakshijha23@gmail.com>
2023-02-12 21:32:53 +0530

    Initial Commit

---
+++ src/views/lifecycle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+  <h1>Lifecycle Hooks example</h1>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+          counter: 0
+        }
+      },
+      beforeCreate () {
+        this.counter=1
+        console.log("Before Create"+this.counter);;
+      },
+      created () {
+        console.log("Created"+this.counter);;;
+      },
+    }
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
